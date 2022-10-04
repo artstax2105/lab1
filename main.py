b122465ae259e57b0cf3597b6b0e502b1aa98481
unknown <artem2003stax1234@gmail.com>
2022-10-04 15:46:11 +0300

    task4/1

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('task2_2')
+    print('task4/1')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
